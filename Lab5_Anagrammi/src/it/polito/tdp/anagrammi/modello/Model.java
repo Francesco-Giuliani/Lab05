43a0bc3b65bc3ed0ea324bb6b99827f8d2834ccc
96fra_000 <96fra_000@172.21.57.128>
2018-04-11 12:25:29 +0200

    Versione iniziale. Analisi del problema fatta in un file di testo. Bisogna implementare il tutto.

---
+++ Lab5_Anagrammi/src/it/polito/tdp/anagrammi/modello/Model.java
@@ -0,0 +1,7 @@
+package it.polito.tdp.anagrammi.modello;
+
+public class Model {
+
+	
+	
+}
---
+++ Lab5_Anagrammi/src/it/polito/tdp/anagrammi/modello/TestModel.java
@@ -0,0 +1,10 @@
+package it.polito.tdp.anagrammi.modello;
+
+public class TestModel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
